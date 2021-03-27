35f52d3cd62fcab431c76f2b0f17e0d4da27a72b
Shashi <shashihghebad3@gmail.com>
2021-03-27 13:17:23 +0530

    [SHASHI]Add:UC3-AppendLinkedList

--- AppendLinkedList/Program.cs
+++ AppendLinkedList/Program.cs
@@ -4,16 +4,16 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace LinkedListSequence
+namespace AppendLinkedList
 {
     class Program
     {
         static void Main(string[] args)
         {
             LinkedList link = new LinkedList();
-            link.Add(70);
-            link.Add(30);
             link.Add(56);
+            link.Add(30);
+            link.Add(70);
             link.Display();
             Console.ReadKey();
         }
