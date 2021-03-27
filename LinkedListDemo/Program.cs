d799ebfc892be5df119dc23afa4df2f00a42bc09
Shashi <shashihghebad3@gmail.com>
2021-03-27 12:49:25 +0530

    [SHASHI]Add:UC1-SimpleLinkList

---
+++ LinkedListDemo/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedListDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LinkedList link = new LinkedList();
+            link.Add(56);
+            link.Add(30);
+            link.Add(76);
+            link.Display();
+            Console.ReadKey();
+        }
+    }
+}
