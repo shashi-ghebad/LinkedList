22f443953ef3ef06329abbd405b28ae17ae49181
Shashi <shashihghebad3@gmail.com>
2021-03-27 13:26:35 +0530

    [SHASHI]Add:UC4-InsertNodes

--- InsertNodes/Node.cs
+++ InsertNodes/Node.cs
@@ -1,12 +1,12 @@
-﻿using System;
+﻿using System;    
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace AppendLinkedList
+namespace InsertNodes
 {
-    class Node
+   class Node
     {
         public int data;
         public Node next;
