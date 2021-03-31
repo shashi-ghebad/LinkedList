17f187c6cd02672ec04e2168ca2a617879661e71
Shashi <shashihghebad3@gmail.com>
2021-03-31 12:00:24 +0530

    [SHASHI]Add:UC5-Delete1Element

--- DeleteFirstElement/Program.cs
+++ DeleteFirstElement/Program.cs
@@ -1,19 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace InsertNodes
+namespace DeleteFirstElement
 {
-   class Program
+    class Program
     {
         static void Main(string[] args)
         {
             LinkedList link = new LinkedList();
             link.Add(56);
             link.Add(70);
-            link.Insert(1,30);
+            link.Insert(1, 30);
+            link.Display();
+            link.RemoveFirstNode();
             link.Display();
             Console.ReadKey();
         }
